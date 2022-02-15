0a0549597a767078daa52cf1b2f963711654a650
Andrew Lamb <andrew@nerdnetworks.org>
2022-02-15 09:22:09 -0500

    Skeleton for comparing

--- parquet9/src/lib.rs
+++ parquet9/src/lib.rs
@@ -1,8 +1,66 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::path::Path;
+use std::fmt::Display;
+use arrow::record_batch::RecordBatchReader;
+use parquet::file::reader::SerializedFileReader;
+use parquet::arrow::{ParquetFileArrowReader, ArrowReader};
+use std::sync::Arc;
+use std::fs::File;
+
+
+#[derive(Debug, Clone)]
+pub struct Error {
+    msg: String
+}
+
+impl std::error::Error for Error {
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error: {}", self.msg)
     }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error { msg: format!("io error: {}", e) }
+    }
+}
+
+impl From<parquet::errors::ParquetError> for Error {
+    fn from(e: parquet::errors::ParquetError) -> Self {
+        Error { msg: format!("parquet error: {}", e) }
+    }
+}
+
+impl From<arrow::error::ArrowError> for Error {
+    fn from(e: arrow::error::ArrowError) -> Self {
+        Error { msg: format!("arrow error: {}", e) }
+    }
+}
+
+
+/// Returns Arrow IPC format (
+///
+/// Decode using https://docs.rs/arrow/9.0.2/arrow/ipc/reader/index.html
+pub fn read_to_serialized_record_batches(path: &Path) -> Result<Vec<u8>, Error> {
+    println!("  Reading {:?} in parquet9", path);
+    let file = File::open(path)?;
+    let file_reader = SerializedFileReader::new(file)?;
+    let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(file_reader));
+
+    //println!("Converted arrow schema is: {}", arrow_reader.get_schema()?);
+
+    let reader = arrow_reader.get_record_reader(2048)?;
+    let mut ipc_writer = arrow::ipc::writer::StreamWriter::try_new(Vec::<u8>::new(), &reader.schema())?;
+
+    for batch in reader {
+        let batch = batch?;
+
+        ipc_writer.write(&batch)?;
+    }
+
+    ipc_writer.finish()?;
+
+    Ok(ipc_writer.into_inner()?)
 }
--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,26 @@ struct ParquetCmp {
 }
 
 
+
 fn main() {
     let args = ParquetCmp::parse();
 
     for file in paths::ParquetIter::new(&args.path) {
         print!("Comparing file {:?}", file);
+
+        let (parquet9_data, parquetnext_data) = match (parquet9::read_to_serialized_record_batches(&file),
+                                                       parquetnext::read_to_serialized_record_batches(&file)) {
+            (Ok(p9), Ok(pn)) => (p9, pn),
+            (Err(p9_err), Err(pn_err)) if p9_err.to_string() == pn_err.to_string() => {
+                //println!("  both readers had problems reading: {}, {}", p9_err, pn_err);
+                println!("  Both readers had same problem reading; skipping file. ");
+                continue;
+            }
+            _ => todo!()
+        };
+
+        println!("  read {} bytes from parquet9, {} from parquet next", parquet9_data.len(), parquetnext_data.len());
+
 
         // TODO actual comparison
 
